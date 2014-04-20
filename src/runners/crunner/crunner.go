e3cf727d2cd7ff59b75c222e95f604b763a4f40b
Xiaofan Li <lixf@cmu.edu>
2014-04-20 15:21:16 -0400

    added parsing capability

--- src/runners/crunner/crunner.go
+++ src/runners/crunner/crunner.go
@@ -8,15 +8,28 @@ import (
     "os"
     "client"
     "rpc/ipaddrs"
+    "io/ioutil"
+    "strings"
+    "strconv"
+    "errors"
 )
-
-
+//initialize flag variables
+var test string
+var time int
 func init() {
-    flag.Usage = func() {
-        fmt.Fprintln(os.Stderr, "The crunner program creates a client.")
-        fmt.Fprintln(os.Stderr, "You can control it with commands: ")
-        fmt.Fprintln(os.Stderr, "UNIMPLEMENTED")
-    }
+    const (
+        defaultTest = "defalt.txt"
+        usageTest = "--benchmark  -b : give benchmark files"
+        defaultTime = 0
+        usageTime = "--time       -t : display timing information"
+    )
+    //for benchmark flags
+    flag.StringVar(&test, "benchmark", defaultTest, usageTest)
+    flag.StringVar(&test, "b", defaultTest, usageTest+ " (-b)")
+
+    //for timing flags
+    flag.IntVar(&time, "time", defaultTime, usageTime)
+    flag.IntVar(&time, "t", defaultTime, usageTime+ " (-t)")
 }
 
 func checkError(err error) {
@@ -40,27 +53,71 @@ func dispPost(client client.RpiClient, key, value string) {
     fmt.Println("[CLIENT] [POST] Status: ", status)
 }
 
-//TODO COMPUTE
-func dispCompute(client client.RpiClient, arg string) {
-    fmt.Println("[CLIENT] [COMPUTE] arg: ", arg)
-    status, res, err := client.Compute(arg)
+//have hashing capability
+func dispCompute(client client.RpiClient, job,key,salt string,cost int) {
+    fmt.Println("[CLIENT] [COMPUTE] job: ",job,"key ",key,"salt ",salt)
+    status, res, err := client.Compute(job,key,salt,cost)
     checkError(err)
     fmt.Println("[CLIENT] [COMPUTE] Status: ", status, ", result: ", res)
 }
 
+func sendReq(client client.RpiClient, requests []string) error {
+    for i:=0; i<len(requests)-1; i++ {
+        req := requests[i]
+        //split on space " "
+        fields := strings.Split(req," ")
+        cmd := fields[0]
+        if strings.Contains(cmd,"COMPUTE") {
+            //parse to a int64
+            cost,err := strconv.ParseInt(fields[4],10,32)
+            checkError(err)
+            //job,key,salt,cost
+            dispCompute(client,fields[1],fields[2],fields[3],int(cost))
+        } else if strings.Contains(cmd,"GET") {
+            //key
+            dispGet(client,fields[1])
+        } else if strings.Contains(cmd,"POST") {
+            //key,val
+            dispPost(client,fields[1],fields[2])
+        } else {
+            fmt.Println(cmd)
+            err := errors.New("Undefined request stream")
+            return err
+        }
+    }
+    return nil
+}
+
 
 func main() {
-    //TODO Command line args
     flag.Parse()
-    /*
-    if flag.NArg() < 2 {
+
+    if flag.NArg() > 0 {
         flag.Usage()
         os.Exit(1)
-    }*/
+    }
 
+    fmt.Println("test file is ",test)
+    fmt.Println("time? ",time)
+
+    //parse the input file specified
+    data, ioErr := ioutil.ReadFile(test)
+    checkError(ioErr)
+
+    fmt.Println("parsing...")
+
+    //do a split on newline to find requests
+    requests := strings.Split(string(data),"\n")
+
+    //network code starts here
     client, err := client.NewClient(ipaddrs.MasterServerHostPort)
     checkError(err)
 
+    //just parse and send the requests now
+    sErr := sendReq(client,requests)
+    checkError(sErr)
+
+    //start sending messages from the file
     dispGet(client, "hihi")
     dispPost(client, "hihi", "asdf")
     dispGet(client, "hihi")
@@ -68,5 +125,6 @@ func main() {
 
     //TODO COMPUTE
     dispCompute(client, "hash","hihi","salt",1)
+    dispGet(client, "hihi")
 }
 
