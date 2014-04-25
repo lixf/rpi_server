d1410e5a5fd36f997ddb96840e5452da845da2df
Sean Klein <smklein@andrew.cmu.edu>
2014-04-25 00:14:24 -0400

    Recommend using generated files which are 10000 lines. Running with three clients on my local machine, achieved 18500 ms performance, +/- one second

--- src/tests/testLocal.sh
+++ src/tests/testLocal.sh
@@ -5,14 +5,16 @@ if [ -z $GOPATH ]; then
     exit 1
 fi
 
-echo "Basic Test: Two workers, one master, one client."
+echo "Basic Test: Two workers, one master, three clients."
 
 WORKER_PORT1=$(((RANDOM % 10000) + 10000))
 WORKER_PORT2=$(((RANDOM % 10000) + 10000))
 WORKER_GO=$GOPATH/src/runners/wrunner/wrunner.go
 MASTER_GO=$GOPATH/src/runners/mrunner/mrunner.go
 CLIENT_GO=$GOPATH/src/runners/crunner/crunner.go
-TEST_FILE=basicGetPost.txt
+TEST_FILE1=$GOPATH/src/tests/longGetPost1.txt
+TEST_FILE2=$GOPATH/src/tests/longGetPost2.txt
+TEST_FILE3=$GOPATH/src/tests/longGetPost3.txt
 
 go run $WORKER_GO -port=$WORKER_PORT1 > "logs/worker1.log" &
 WORKER1_PID=$!
@@ -24,12 +26,29 @@ go run $MASTER_GO -N=2 > "logs/master.log" &
 MASTER_PID=$!
 
 sleep 3
-go run $CLIENT_GO -b=$TEST_FILE > "logs/client.log" &
-sleep 4
+echo "Starting timer:"
+T="$(date +%s%N)"
 
+# Do some work here
+go run $CLIENT_GO -b=$TEST_FILE1 > "logs/client1.log" &
+CLIENT_PID1=$!
+go run $CLIENT_GO -b=$TEST_FILE2 > "logs/client2.log" &
+CLIENT_PID2=$!
+go run $CLIENT_GO -b=$TEST_FILE3 > "logs/client3.log" &
+CLIENT_PID3=$!
 
-cat logs/master.log
+echo "[TEST] Waiting for clients to finish"
+wait $CLIENT_PID1
+echo "[TEST] Client 1 finished."
+wait $CLIENT_PID2
+echo "[TEST] Client 2 finished."
+wait $CLIENT_PID3
+echo "[TEST] Client 3 finished."
 
+T="$(($(date +%s%N)-$T))"
+M="$((T/1000000))"
+echo "[TEST] Time Elapsed in milliseconds: ${M}"
+echo "[TEST] Correctness not yet checked"
 echo "[TEST] Finish. Control + C to kill master and workers."
 
 wait $WORKER1_PID
