1da7347da8112d5ad3c385a6bf0b3b17fc6886d6
Sean Klein <smklein@andrew.cmu.edu>
2014-05-05 15:28:13 -0400

    Updated test generation script to take command line arguments, adjust probabilities of test types accordingly

--- src/tests/generateCommandFile.py
+++ src/tests/generateCommandFile.py
@@ -3,6 +3,9 @@ import random
 import string
 
 cmds = ["GET", "POST", "HASH"]
+prob = dict()
+for cmd in cmds:
+    prob[cmd] = 0
 
 num_args = len(sys.argv) - 1
 alphabet = string.lowercase
@@ -12,17 +15,35 @@ def randomWord(length = 0):
         length = random.randint(8, 12)
     return ''.join(random.choice(alphabet) for n in xrange(length))
 
-if (num_args != 2):
-    print "python generateCommandFile.py [OUTPUT FILE] [NUM COMMANDS]"
+if (num_args != 3):
+    print "python generateCommandFile.py [OUTPUT FILE] [NUM COMMANDS] ['hash'/'basic']"
     exit(0)
 
 filename = sys.argv[1]
 numCommands = int(sys.argv[2])
+cmdType = sys.argv[3]
+
+#... just... keep the probabilities to two digits. 
+if cmdType == 'hash':
+    print "HASH -- 5% GET, 5% POST, 90% HASH"
+    prob["GET"] = 0.05
+    prob["POST"] = 0.05
+    prob["HASH"] = 0.90
+elif cmdType == 'basic':
+    print "BASIC -- 40% GET, 40$ POST, 20% HASH"
+    prob["GET"] = 0.40
+    prob["POST"] = 0.40
+    prob["HASH"] = 0.20
+
+cmdPadded = []
+for key in prob:
+    for i in xrange(int(prob[key] * 100)):
+        cmdPadded.append(key)
 
 f = open(filename, 'w')
 
 for i in xrange(numCommands):
-    cmd = random.choice(cmds)
+    cmd = random.choice(cmdPadded)
     args = ""
     if cmd == "GET":
         args = randomWord()
