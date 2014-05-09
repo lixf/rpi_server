b64c08e3217cb930947dc26c2597dfb736a1846e
Sean Klein <smklein@andrew.cmu.edu>
2014-05-08 21:40:56 -0400

    tests for basic, hash, bandwidth

--- src/tests/testPi_10.sh
+++ src/tests/testPi_10.sh
@@ -27,7 +27,7 @@ go run $CLIENT_GO -b=$TESTS/pure_basic_25_8.txt > $TESTS/logs/client8_pb25_10.lo
 CLIENT_PID8=$!
 go run $CLIENT_GO -b=$TESTS/pure_basic_25_9.txt > $TESTS/logs/client9_pb25_10.log &
 CLIENT_PID9=$!
-go run $CLIENT_GO -b=$TESTS/pure_basic_25_10.txt > $TESTS/logs/clientx_pb25_10.log &
+go run $CLIENT_GO -b=$TESTS/pure_basic_25_A.txt > $TESTS/logs/clientA_pb25_10.log &
 CLIENT_PID10=$!
 
 echo "[TEST] Waiting for client 1 to finish"
@@ -48,12 +48,34 @@ echo "[TEST] Waiting for client 8 to finish"
 wait $CLIENT_PID8
 echo "[TEST] Waiting for client 9 to finish"
 wait $CLIENT_PID9
-echo "[TEST] Waiting for client 10 to finish"
+echo "[TEST] Waiting for client A to finish"
 wait $CLIENT_PID10
 
 T="$(($(date +%s%N)-$T))"
 M="$((T/1000000))"
 echo "[TEST] Time Elapsed in milliseconds: ${M}"
+
+client1Time="$(tail -n 1 logs/client1_pb25_10.log | awk 'NF>1{print $NF}')"
+client2Time="$(tail -n 1 logs/client2_pb25_10.log | awk 'NF>1{print $NF}')"
+client3Time="$(tail -n 1 logs/client3_pb25_10.log | awk 'NF>1{print $NF}')"
+client4Time="$(tail -n 1 logs/client4_pb25_10.log | awk 'NF>1{print $NF}')"
+client5Time="$(tail -n 1 logs/client5_pb25_10.log | awk 'NF>1{print $NF}')"
+client6Time="$(tail -n 1 logs/client6_pb25_10.log | awk 'NF>1{print $NF}')"
+client7Time="$(tail -n 1 logs/client7_pb25_10.log | awk 'NF>1{print $NF}')"
+client8Time="$(tail -n 1 logs/client8_pb25_10.log | awk 'NF>1{print $NF}')"
+client9Time="$(tail -n 1 logs/client9_pb25_10.log | awk 'NF>1{print $NF}')"
+clientATime="$(tail -n 1 logs/clientA_pb25_10.log | awk 'NF>1{print $NF}')"
+echo $client1Time
+echo $client2Time
+echo $client3Time
+echo $client4Time
+echo $client5Time
+echo $client6Time
+echo $client7Time
+echo $client8Time
+echo $client9Time
+echo $clientATime
+
 echo "[TEST] Correctness not yet checked"
 echo "[TEST] Finished."
 
---
+++ src/tests/testPi_basic_variable.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Basic Test: Three workers, one master, three clients."
+
+echo "[TEST] Starting timer:"
+T="$(date +%s%N)"
+
+echo "[TEST] CLIENT (run locally):"
+CLIENT_GO=$GOPATH/src/runners/crunner/crunner.go
+TESTS=$GOPATH/src/tests
+
+NUM_CLIENTS=3
+
+for (( i = 0 ; i < $NUM_CLIENTS ; i++ ))
+do
+    go run $CLIENT_GO -b=$TESTS/pure_basic_25_"$i".txt > $TESTS/logs/client"$i"_pb25_10.log &
+    CLIENT_PID[$i]=$!
+done
+
+for (( i = 0 ; i < $NUM_CLIENTS ; i++ ))
+do
+    echo "[TEST] Waiting for client $i to finish"
+    wait ${CLIENT_PID[$i]}
+done
+
+T="$(($(date +%s%N)-$T))"
+M="$((T/1000000))"
+echo "[TEST] Time Elapsed in milliseconds: ${M}"
+
+for (( i = 0 ; i < $NUM_CLIENTS ; i++ ))
+do
+    echo "$(tail -n 1 logs/client"$i"_pb25_10.log | awk 'NF>1{print $NF}')"
+done
+
+echo "[TEST] Correctness not yet checked"
+echo "[TEST] Finished."
+
